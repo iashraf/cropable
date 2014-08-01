47f531e9f2b74a2b286ec9c62a8ce445f841266e
Richard Kimber <richard@dogma.co.uk>
2014-08-01 11:25:02 +0100

    Made input more flexible

--- app/inputs/image_input.rb
+++ app/inputs/image_input.rb
@@ -6,13 +6,12 @@ class ImageInput
       output = label_html <<
       builder.file_field(method, input_html_options)
 
-
       unless @object.new_record? or @object.send( @method ).blank?
         cropable = @object.respond_to? "#{method}_x"
 
         output << "<div class=\"image-input-container\">".html_safe
 
-        output << "<a class=\"modal\" href=\"#{ Rails.application.routes.url_helpers.crop_admin_product_path( @object, image: method, style: @options[:style] ) }\">".html_safe if cropable
+        output << "<a class=\"modal\" href=\"#{ Rails.application.routes.url_helpers.send( "crop_admin_#{@object.class.name.underscore}_path", @object, { image: method, style: @options[:style] } ) }\">".html_safe if cropable
 
         output << "<img class=\"image-input-image\" src=\"#{@object.send( @method ).url @options[:style] }\" />".html_safe
 
